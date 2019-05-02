c80c408e6ead8ba1ab8d0821b863b5a91e8bcb4a
Sean Padden <sean.padden7@gmail.com>
2019-05-02 15:41:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,20 +1,24 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  my_array = ["hey", "what's up", "ok cool", "bye!"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["hey", "what's up", "ok cool", "bye!"]
+    array >> ["oh"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  this_array = ["hi", "sup"]
+  this_array.unshift("hello?")
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["hi", "sup"]
+  array.pop("element")
   
 end
 
