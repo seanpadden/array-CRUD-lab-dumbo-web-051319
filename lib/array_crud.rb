5c6fb2cbc7b2faffd6375adab9923fa3446db890
Sean Padden <sean.padden7@gmail.com>
2019-05-02 15:41:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
     array = ["hey", "what's up", "ok cool", "bye!"]
-    array >> ["oh"]
+    array >> "oh"
 end
 
 def add_element_to_start_of_array(array, element)
